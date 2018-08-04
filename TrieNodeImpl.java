e7cee3c995153c050dca15e316ddeb17a9de65e5
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2018-08-04 14:15:57 -0700

    Add fuzzy completion to my trie

--- TrieNodeImpl.java
+++ TrieNodeImpl.java
@@ -2,38 +2,59 @@ public class TrieNodeImpl {
   public static void main(String[] args) {
     TrieNode tn = new TrieNode();
 
-    addWord("Hello", tn);
-    addWord("Adam", tn);
-    addWord("adam", tn);
-    addWord("banana", tn);
-    addWord("dinosaur", tn);
-    addWord("play", tn);
-    addWord("hockey", tn);
-    addWord("magic", tn);
+    // addWord("Hello", tn);
+    // addWord("Adam", tn);
+    // addWord("adam", tn);
+    // addWord("banana", tn);
+    // addWord("dinosaur", tn);
+    // addWord("play", tn);
+    // addWord("hockey", tn);
+    // addWord("magic", tn);
 
     // printWords("", tn);
 
-    removeWord("Hello", tn);
-    removeWord("Adam", tn);
-    removeWord("adam", tn);
-    removeWord("banana", tn);
-    removeWord("dinosaur", tn);
-    removeWord("play", tn);
-    removeWord("hockey", tn);
-    removeWord("magic", tn);
+    // removeWord("Hello", tn);
+    // removeWord("Adam", tn);
+    // removeWord("adam", tn);
+    // removeWord("banana", tn);
+    // removeWord("dinosaur", tn);
+    // removeWord("play", tn);
+    // removeWord("hockey", tn);
+    // removeWord("magic", tn);
 
     // printWords("", tn);
 
-    addWord("Hello", tn);
-    addWord("Adam", tn);
-    addWord("adam", tn);
-    addWord("adambanana", tn);
-    addWord("adamdinosaur", tn);
-    addWord("adamplay", tn);
-    addWord("adamhockey", tn);
-    addWord("adammagic", tn);
+    // addWord("Hello", tn);
+    // addWord("Adam", tn);
+    // addWord("adam", tn);
+    // addWord("adambanana", tn);
+    // addWord("adamdinosaur", tn);
+    // addWord("adamplay", tn);
+    // addWord("adamhockey", tn);
+    // addWord("adammagic", tn);
 
-    printCompletion("adam", tn);
+    // printCompletion("", tn);
+
+    addWord("Aadam", tn);
+    addWord("Abadam", tn);
+    addWord("Adhjl", tn);
+    addWord("Adoijoi", tn);
+    addWord("Adaksjns", tn);
+    addWord("Adaoijoi", tn);
+    addWord("Adamjbhb", tn);
+    addWord("Adam", tn);
+    addWord("Adameiwuei", tn);
+    addWord("aadam", tn);
+    addWord("abadam", tn);
+    addWord("adhjl", tn);
+    addWord("adoijoi", tn);
+    addWord("adaksjns", tn);
+    addWord("adaoijoi", tn);
+    addWord("adamjbhb", tn);
+    addWord("adam", tn);
+    addWord("adameiwuei", tn);
+
+    printFuzzyCompletion("Adam", tn);
   }
 
   private static void addWord(String word, TrieNode tn) {
@@ -81,16 +102,29 @@ public class TrieNodeImpl {
     TrieNode cur = tn;
     int ci = 0;
     while (true) {
-      if (ci == word.length() - 1) break;
-
-      if (cur.letters[word.charAt(ci)] == null) return;
-
+      if (ci == word.length()) break;
+      if (cur == null) return;
       cur = cur.letters[word.charAt(ci)];
       ci++;
     }
+    printWords(word, cur);
+  }
 
-    for (int i = 0; i < 256; i++) {
-      printWords(word, cur.letters[i]);
+  private static void printFuzzyCompletion(String word, TrieNode tn) {
+    if (tn == null) return;
+
+    int ci = 0;
+    TrieNode cur = tn.letters[word.charAt(ci)];
+    while (true) {
+      if (ci == word.length()) break;
+      if (cur == null) break;
+      for (int i = 0; i < 256; i++) {
+        if (i != (int) word.charAt(ci)) {
+          printWords(word.substring(0, ci + 1) + (char) i, cur.letters[i]);
+        }
+      }
+      cur = cur.letters[word.charAt(ci)];
+      ci++;
     }
   }
 
